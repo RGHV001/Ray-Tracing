9358f86035ab57d59e7900973b6f2dea8642f76e
Rogerio H. Vitorassi <95109276+RGHV001@users.noreply.github.com>
2023-10-10 19:08:44 -0300

    Add files via upload

---
+++ 2bim/main.cpp
@@ -0,0 +1,54 @@
+#include "rtweekend.h"           // Inclui um arquivo de cabeçalho com funções e constantes úteis.
+#include "color.h"               // Inclui um arquivo de cabeçalho para lidar com cores.
+#include "hittable_list.h"       // Inclui um arquivo de cabeçalho para a lista de objetos atingíveis.
+#include "sphere.h"              // Inclui um arquivo de cabeçalho para lidar com esferas.
+#include <iostream>              // Inclui a biblioteca de entrada/saída padrão do C++.
+#include "camera.h"
+
+
+color ray_color(const ray& r, const hittable& world, int depth) {
+ hit_record rec;
+ // If we've exceeded the ray bounce limit, no more light is gathered.
+ if (depth <= 0)
+ return color(0,0,0);
+ if (world.hit(r, 0.001, infinity, rec)) {
+ point3 target = rec.p + rec.normal + random_in_unit_sphere();
+ return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+ }
+ vec3 unit_direction = unit_vector(r.direction());
+ auto t = 0.5*(unit_direction.y() + 1.0);
+ return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+int main() {
+ // Image
+ const auto aspect_ratio = 16.0 / 9.0;
+ const int image_width = 400;
+ const int image_height = static_cast<int>(image_width / aspect_ratio);
+ const int samples_per_pixel = 100;
+ const int max_depth = 50;
+
+ // World
+ hittable_list world;
+ world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+ world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+ // Camera
+ camera cam;
+ // Render
+ std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+ for (int j = image_height-1; j >= 0; --j) {
+ std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+ for (int i = 0; i < image_width; ++i) {
+ color pixel_color(0, 0, 0);
+ for (int s = 0; s < samples_per_pixel; ++s) {
+ auto u = (i + random_double()) / (image_width-1);
+ auto v = (j + random_double()) / (image_height-1);
+ ray r = cam.get_ray(u, v);
+ pixel_color += ray_color(r, world, max_depth);
+ }
+ write_color(std::cout, pixel_color, samples_per_pixel);
+ }
+ }
+ std::cerr << "\nDone.\n";
+}
+
